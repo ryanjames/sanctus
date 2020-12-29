a68befed60e67d791d966c3b581ed57d4d471e0e
ryanjames <ryan@ryanjam.es>
2020-12-28 18:39:02 -0800

    Reset to boilerplate

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,11 @@
-require("dotenv").config({
-  path: `.env`,
-});
-
 module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
   plugins: [
-    "gatsby-plugin-react-helmet",
+    `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -12,28 +13,22 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: "gatsby-source-strapi",
-      options: {
-        apiURL: process.env.API_URL || "http://localhost:1337",
-        contentTypes: ["article", "category", "writer"],
-        singleTypes: [`homepage`, `global`],
-        queryLimit: 1000,
-      },
-    },
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sharp",
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: "gatsby-starter-default",
-        short_name: "starter",
-        start_url: "/",
-        background_color: "#663399",
-        theme_color: "#663399",
-        display: "minimal-ui",
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    "gatsby-plugin-offline",
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
   ],
-};
+}
--- src/components/seo.js
+++ src/components/seo.js
@@ -1,145 +1,89 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Helmet } from "react-helmet";
-import { useStaticQuery, graphql } from "gatsby";
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
 
-const SEO = ({ seo = {} }) => {
-  const { strapiGlobal } = useStaticQuery(query);
-  const { defaultSeo, siteName, favicon } = strapiGlobal;
+import React from "react"
+import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
 
-  // Merge default and page-specific SEO values
-  const fullSeo = { ...defaultSeo, ...seo };
-
-  const getMetaTags = () => {
-    const tags = [];
-
-    if (fullSeo.metaTitle) {
-      tags.push(
-        {
-          property: "og:title",
-          content: fullSeo.metaTitle,
-        },
-        {
-          name: "twitter:title",
-          content: fullSeo.metaTitle,
+function SEO({ description, lang, meta, title }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+          }
         }
-      );
-    }
-    if (fullSeo.metaDescription) {
-      tags.push(
-        {
-          name: "description",
-          content: fullSeo.metaDescription,
-        },
-        {
-          property: "og:description",
-          content: fullSeo.metaDescription,
-        },
-        {
-          name: "twitter:description",
-          content: fullSeo.metaDescription,
-        }
-      );
-    }
-    if (fullSeo.shareImage) {
-      const imageUrl =
-        (process.env.GATSBY_ROOT_URL || "http://localhost:8000") +
-        fullSeo.shareImage.publicURL;
-      tags.push(
-        {
-          name: "image",
-          content: imageUrl,
-        },
-        {
-          property: "og:image",
-          content: imageUrl,
-        },
-        {
-          name: "twitter:image",
-          content: imageUrl,
-        }
-      );
-    }
-    if (fullSeo.article) {
-      tags.push({
-        property: "og:type",
-        content: "article",
-      });
-    }
-    tags.push({ name: "twitter:card", content: "summary_large_image" });
+      }
+    `
+  )
 
-    return tags;
-  };
-
-  const metaTags = getMetaTags();
+  const metaDescription = description || site.siteMetadata.description
+  const defaultTitle = site.siteMetadata?.title
 
   return (
     <Helmet
-      title={fullSeo.metaTitle}
-      titleTemplate={`%s | ${siteName}`}
-      link={[
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      meta={[
         {
-          rel: "icon",
-          href: favicon.publicURL,
+          name: `description`,
+          content: metaDescription,
         },
         {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css?family=Staatliches",
+          property: `og:title`,
+          content: title,
         },
         {
-          rel: "stylesheet",
-          href:
-            "https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css",
-        },
-      ]}
-      script={[
-        {
-          src:
-            "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js",
+          property: `og:description`,
+          content: metaDescription,
         },
         {
-          src:
-            "https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js",
+          property: `og:type`,
+          content: `website`,
         },
         {
-          src: "https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js",
+          name: `twitter:card`,
+          content: `summary`,
         },
-      ]}
-      meta={metaTags}
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata?.author || ``,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
     />
-  );
-};
-
-export default SEO;
-
-SEO.propTypes = {
-  title: PropTypes.string,
-  description: PropTypes.string,
-  image: PropTypes.string,
-  article: PropTypes.bool,
-};
+  )
+}
 
 SEO.defaultProps = {
-  title: null,
-  description: null,
-  image: null,
-  article: false,
-};
+  lang: `en`,
+  meta: [],
+  description: ``,
+}
 
-const query = graphql`
-  query {
-    strapiGlobal {
-      siteName
-      favicon {
-        publicURL
-      }
-      defaultSeo {
-        metaTitle
-        metaDescription
-        shareImage {
-          publicURL
-        }
-      }
-    }
-  }
-`;
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+}
+
+export default SEO
