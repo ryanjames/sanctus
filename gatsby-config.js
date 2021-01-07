702999cff287517bb18fa3c0cb6d3a1f816d4e7f
ryanjames <ryan@ryanjam.es>
2021-01-06 20:55:56 -0800

    Change limit

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
         extensions: [".mp3"],
         path: "/DK Library 256 mp3s/Website",
         recursive: false,
-        limit: 1500,
+        limit: 1000,
         createFolderNodes: false,
       },
     },
