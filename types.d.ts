9b79f963cf5663d00dd0f6e526bd21b8ab0cc804
ryanjames <ryan@ryanjam.es>
2021-01-14 10:45:01 -0800

    Clean up packages

--- types.d.ts
+++ types.d.ts
@@ -1,7 +1,4 @@
-declare module "remark-parse"
-declare module "remark-rehype"
-declare module "rehype-stringify"
-declare module "react-wavesurfer"
+declare module "hex2rgba"
 
 interface Window {
   player: {
