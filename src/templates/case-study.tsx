b3383f5156ed272772976d2c9ffcf24c60bb221c
ryanjames <ryan@ryanjam.es>
2021-05-07 14:04:37 -0700

    Non-video case study image

--- src/templates/case-study.tsx
+++ src/templates/case-study.tsx
@@ -8,6 +8,7 @@ import ActiveTrackProvider from "../contexts/ActiveTrackContext"
 import PageLink from "../components/PageLink"
 import tw from "twin.macro"
 import { getCaseStudy } from "../models/case-study"
+import Img, { FluidObject } from "gatsby-image"
 import { Helmet } from "react-helmet"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import formattingOptions from "../utils/formattingOptions"
@@ -43,13 +44,20 @@ const CaseStudyPage: React.FC<Props> = ({ data }) => {
       <Helmet>
         <script src="https://unpkg.com/wavesurfer.js"></script>
       </Helmet>
-      {caseStudy.video && (
+      {caseStudy.video ? (
         <div tw="lg:-mt-32 relative z-10">
           <Video autoplay={false} src={caseStudy.video} poster={caseStudy.image} color={caseStudy.color} />
         </div>
+      ) : (
+        <div
+          tw="lg:-mt-32 mb-24 h-40vh relative z-10 bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${caseStudy.image.src})`,
+          }}
+        />
       )}
       <Container className="caseStudy-content" tw="pb-32">
-        <Col tw="md:w-1/4 flex flex-wrap md:block py-12 md:py-0 text-gray-500">
+        <Col tw="md:w-1/4 flex flex-wrap md:block pb-12 md:pb-0 text-gray-500">
           <div tw="sm:w-1/2 md:w-full pr-8">
             <label tw="text-xs pt-9 uppercase tracking-widest mb-4">Client</label>
             <p>{caseStudy.client}</p>
