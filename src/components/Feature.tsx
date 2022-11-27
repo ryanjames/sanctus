872de74ffa7b0178ee0fd53e037fdecea3f50379
Ryan James <ryan@ryanjam.es>
2022-11-27 14:00:26 -0800

    Add and style feature component

---
+++ src/components/Feature.tsx
@@ -0,0 +1,91 @@
+import React from "react"
+import styled from "@emotion/styled"
+import tw from "twin.macro"
+import Img, { FluidObject } from "gatsby-image"
+import PageLink from "./PageLink"
+import Container, { Col } from "./Container"
+
+type Props = {
+  className?: string,
+  image: FluidObject,
+  title: string,
+  body: string,
+  button?: {
+    link: string,
+    label: string,
+  },
+  orientation?: string,
+}
+
+const Feature: React.FC<Props> = ({ className, image, title, body, button, orientation = "left" }) => {
+  return (
+    <StyledFeature data-orientation={orientation}>
+      <div className="feature-text-space" />
+      <Img className="feature-image" fluid={image} />
+      <div className="feature-text">
+        <Container className="feature-text-inner">
+          <Col tw="w-full xs:w-1/2">
+            <h2>{title}</h2>
+            <div>{body}</div>
+            {button && (
+              <PageLink to={button.link} className="feature-button">
+                {button.label}
+              </PageLink>
+            )}
+          </Col>
+        </Container>
+      </div>
+    </StyledFeature>
+  )
+}
+
+const StyledFeature = styled.section`
+  ${tw``}
+  padding: 3em 0;
+  position: relative;
+  @media (min-width: 480px) {
+    display: flex;
+    align-items: center;
+  }
+  &[data-orientation="right"] {
+    flex-direction: row-reverse;
+    .feature-text-inner > div {
+      display: flex;
+      flex-direction: row-reverse;
+    }
+  }
+  .feature-text-space, .feature-image {
+    flex: 1;
+  }
+  .feature-button {
+    padding: 0.8em 1em;
+    border-radius: 6px;
+    background: #689CB2;
+    display: inline-block;
+    margin-top: 16px;
+  }
+  .feature-image {
+    @media (min-width: 480px) {
+      min-height: 450px;
+    }
+    max-height: 650px;
+  }
+  .feature-text {
+    padding-top: 2em;
+    @media (min-width: 480px) {
+      padding-top: 0;
+      position: absolute;
+      top: 0;
+      width: 100%;
+      height: 100%;
+    }
+    display: flex;
+    justify-content: center;
+    h2 {
+      font-weight:700;
+      font-size: 1.8em;
+    }
+  }
+
+`
+export default Feature
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "@emotion/styled"
 import tw from "twin.macro"
 import Layout from "../components/Layout"
+import Feature from "../components/Feature"
 import Video from "../components/Video"
 
 const IndexPage: React.FC = () => {
@@ -17,6 +18,19 @@ const IndexPage: React.FC = () => {
     },
     color: "#000000",
 
+  }
+
+  const feature = {
+    image: {
+      "base64": "data:image/jpeg;base64,/9j/2wBDABALDA4MChAODQ4SERATGCgaGBYWGDEjJR0oOjM9PDkzODdASFxOQERXRTc4UG1RV19iZ2hnPk1xeXBkeFxlZ2P/2wBDARESEhgVGC8aGi9jQjhCY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2NjY2P/wgARCAALABQDASIAAhEBAxEB/8QAFgABAQEAAAAAAAAAAAAAAAAAAwAC/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAH/2gAMAwEAAhADEAAAAQGGMxR//8QAGhAAAQUBAAAAAAAAAAAAAAAAAQACAxEhEv/aAAgBAQABBQKN1GR/RpQaQMX/xAAUEQEAAAAAAAAAAAAAAAAAAAAQ/9oACAEDAQE/AT//xAAUEQEAAAAAAAAAAAAAAAAAAAAQ/9oACAECAQE/AT//xAAYEAADAQEAAAAAAAAAAAAAAAABEBFRAP/aAAgBAQAGPwK5xOoV/wD/xAAYEAEBAQEBAAAAAAAAAAAAAAABABEhUf/aAAgBAQABPyESHPUm+DHEAA0YFQ3/2gAMAwEAAgADAAAAEPg//8QAFREBAQAAAAAAAAAAAAAAAAAAEBH/2gAIAQMBAT8Qh//EABQRAQAAAAAAAAAAAAAAAAAAABD/2gAIAQIBAT8QP//EABoQAQADAQEBAAAAAAAAAAAAAAEAESFBYaH/2gAIAQEAAT8Qdse/URKHaKfPkyUjZewUlgR6RZS0uJu9Z//Z",
+      "aspectRatio": 1.7751479289940828,
+      "src": "/static/28e5242f1ab52422c58973c247f13b67/0e329/persuit-feature.jpg",
+      "srcSet": "/static/28e5242f1ab52422c58973c247f13b67/4fe8c/persuit-feature.jpg 600w,\n/static/28e5242f1ab52422c58973c247f13b67/47498/persuit-feature.jpg 1200w,\n/static/28e5242f1ab52422c58973c247f13b67/0e329/persuit-feature.jpg 1600w",
+      "sizes": "(max-width: 1600px) 100vw, 1600px",
+    },
+    title: "Audio for Experiences",
+    slug: "/work/pursuit",
+    body: "Nullam id dolor id nibh ultricies vehicula ut id elit. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum.",
   }
 
   return (
@@ -37,13 +51,32 @@ const IndexPage: React.FC = () => {
           </svg>
         </StyledContinue>
       </StyledReelContainer>
-      <p>Case Study</p>
-      <p>Case Study</p>
-      <p>Case Study</p>
-      <p>Case Study</p>
-      <p>Agencies</p>
-      <p>Clients</p>
-      <p>Contact CTA</p>
+      <Feature 
+        title={feature.title}
+        image={feature.image}
+        button={{ link: feature.slug, label: "View Case Study"} }
+        body={feature.body} 
+      />
+      <Feature 
+        orientation="right"
+        title={feature.title}
+        image={feature.image}
+        button={{ link: feature.slug, label: "View Case Study"} }
+        body={feature.body} 
+      />
+      <Feature 
+        title={feature.title}
+        image={feature.image}
+        button={{ link: feature.slug, label: "View Case Study"} }
+        body={feature.body} 
+      />
+      <Feature 
+        orientation="right"
+        title={feature.title}
+        image={feature.image}
+        button={{ link: feature.slug, label: "View Case Study"} }
+        body={feature.body} 
+      />
     </StyledLayout>
   )
 }
